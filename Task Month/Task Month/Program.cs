610ec2d497b6dd2cbd9b6b41b491ad3a05aaecf6
unknown <qjale>
2022-03-03 20:40:03 +0400

    Task added

---
+++ Task Month/Task Month/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_Month
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task
+            string month = "june";
+            if (month == "december" || month == "january" || month == "february")
+            {
+                Console.WriteLine("winter");
+            }
+            else if (month == "march" || month == "april" || month == "may")
+            {
+                Console.WriteLine("spring");
+            }
+            else if (month == "june" || month == "july" || month == "august")
+            {
+                Console.WriteLine("summer");
+            }
+            else if (month == "september" || month == "october" || month == "november")
+            {
+                Console.WriteLine("autumn");
+            }
+            else
+            {
+                Console.WriteLine("wrong input");
+            }
+            #endregion Task
+        }
+    }
+}
